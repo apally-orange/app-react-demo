f6f2b463604f3d22d9691776d3909953f14dbedb
apally <adrien.pally@orange.com>
2026-02-11 11:17:47 +0100

    add auth todos by user

--- src/routes/_authenticated/todos/index.tsx
+++ src/routes/_authenticated/todos/index.tsx
@@ -1,18 +1,65 @@
+import { queryOptions, useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authenticated/todos/')({
 	component: RouteComponent,
+	loader: ({ context: { queryClient, auth } }) =>
+		queryClient.ensureQueryData(todosQueryOptions(auth.user?.id ?? '')),
 })
 
+const todosQueryOptions = (userId: string) =>
+	queryOptions({
+		queryFn: () => {
+			const token = localStorage.getItem('auth-token')
+
+			return fetch(`https://dummyjson.com/auth/users/${userId}/todos`, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+					'Content-Type': 'application/json',
+				},
+				method: 'GET',
+			}).then((res) => res.json())
+		},
+		queryKey: ['todos'],
+	})
+
 function RouteComponent() {
-    const { auth } = Route.useRouteContext()
-    
-	return (<div>
-        <p className="text-gray-600">
-          Hello, <strong>{auth.user?.username}</strong>! You are successfully
-          authenticated.
-        </p>
-    </div>)
+	const { auth } = Route.useRouteContext()
+
+	return (
+		<div>
+			<h1>
+				Hello, <strong>{auth.user?.username}</strong>!
+			</h1>
+			<Todoslist />
+		</div>
+	)
 }
 
+type Todo = {
+	id: string
+	todo: string
+	completed: boolean
+	userId: string
+}
 
+function Todoslist() {
+	const { auth } = Route.useRouteContext()
+	const { data } = useSuspenseQuery(todosQueryOptions(auth.user.id))
+
+	const todos = data.todos as Todo[]
+
+	return (
+		<ul>
+			{todos.map((todo) => (
+				<li
+					key={todo.id}
+					style={{ alignItems: 'center', display: 'flex', gap: 8 }}>
+					<span>{todo.completed ? '✅' : '⬜'}</span>
+					<span>{todo.todo}</span>
+				</li>
+			))}
+		</ul>
+	)
+}
+	
